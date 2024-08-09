dc8bbd7db58246fd5e0fd37e9c9ba5106dade3e0
Giridhar <bhujbalgiridhar@gmail.com>
2024-08-09 15:09:28 +0530

    change in file

--- Pattern.java
+++ Pattern.java
@@ -6,9 +6,6 @@ public class Pattern {
 
 	public static void main(String[] args) {
 		int num=6;
-		//Scanner sc = new Scanner (System.in);
-		//System.out.println("enter num :- ");
-		//int num=sc.nextInt();
 		
 		for(int i=1;i<=num;i++)
 		{ 
@@ -26,26 +23,7 @@ public class Pattern {
 		}
 			System.out.println();
 		}
-//		int n=4;
-//		
-//		for(int i=1;i<=n;i++) {
-//			for(int j=1; j<=i; j++) {
-//				System.out.print("*");
-//			}System.out.println();
-//
-//		}
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("print your first number :-");
-//		int n=sc.nextInt();
-//		//int n=5;
-//		//int m=4;
-//		for(int i=n; i>=1; i--)  {
-//			for(int j=1; j<=i; j++)
-//			 {
-//				System.out.print("*");
-//			} System.out.println();
-//			
-//		}
+
 	}
 
 }
